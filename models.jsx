ac9966620636125fc96d8e97cf17d776c8846527
Michael Hinstridge <michael.hinstridge@artsalliancemedia.com>
2015-02-12 13:31:08 +0000

    Inital version that gets to the game page

--- models.jsx
+++ models.jsx
@@ -18,19 +18,53 @@ var Team = React.createClass({
     },
     getInitialState: function() {
         return {
+            name: TEAM_DEFAULT_NAME,
             resources: [],
             morale: Math.random() * (MORALE_MAX - MORALE_MIN) + MORALE_MIN,
             story_points: 0,
             stories_completed: 0
         }
+    },
+    addResource: function(resource) {
+        this.state.resources.push(resource);
     }
 });
 
 var Game = React.createClass({
+    createTeam: function() {
+        team = new Team();
+        console.log(team);
+        this.advanceState();
+    },
+    advanceState: function() {
+        this.setState({status: this.state.status+1});
+    },
     render: function() {
-        return (
-            <b>Welcome to the game! (version {this.props.version})</b>
-        );
+        if (this.state.status == GAME_NOT_STARTED) {
+            return (
+                <div><b>Welcome to the game! (version {this.props.version})</b>
+
+                <p><button onClick={this.advanceState}>Start the game</button></p></div>
+            );
+        }
+        else if (this.state.status == CREATE_TEAM) {
+            return (
+                    <table>
+                        <tr><td>Team name</td><td><input name="team_name" value={TEAM_DEFAULT_NAME} /></td></tr>
+                        <tr><td colspan="2"><button onClick={this.createTeam}>Create</button></td></tr>
+                    </table>
+            );
+        } else {
+            return (
+                <h2>Leo please add details</h2>
+            );
+        }
+    },
+    getInitialState: function() {
+        return {
+            team: null,
+            status: GAME_NOT_STARTED
+        }
     }
 });
 
