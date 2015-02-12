1a5b3cac12facf41d42ec4dd9aa7d2dfdd5884b4
Michael Hinstridge <michael.hinstridge@artsalliancemedia.com>
2015-02-12 16:28:41 +0000

    Moar styling

--- oregon.js
+++ oregon.js
@@ -1,5 +1,6 @@
 'use strict';
 /* global EVENTS, $*/
+var TICKS = 0;
 
 var team = {
     //name: velocity
@@ -46,6 +47,10 @@ var teamStatus = function() {
 };
 
 var tick = function() {
+    TICKS++;
+    if (TICKS > 0) {
+        $('#title').hide();
+    }
     // disable the next sprint button
     $('#tick').attr("disabled", true);
 
@@ -69,3 +74,10 @@ var clean_up_dom = function() {
     $('#description').empty();
     $('#choices').empty();
 };
+
+// This is to fix a wierd bug in firefox whereby refreshing the page does not reset the button state
+window.onload = function() {
+    if (TICKS == 0) {
+        $('#tick').attr("disabled", false);
+    }
+}
